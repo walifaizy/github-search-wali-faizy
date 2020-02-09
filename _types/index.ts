4b02e24f7a6d06d4c024ef8295691cf3ce20eb45
walifaizy <faizy.wali@gmail.com>
2020-02-10 00:42:40 +0400

    .

--- _types/index.ts
+++ _types/index.ts
@@ -5,3 +5,7 @@ export type TUserData = {
   name: string;
   html_url: string;
 };
+
+export type TRepos = {
+  repos: object[] | null;
+};
--- components/searchContainer/searchContainer.tsx
+++ components/searchContainer/searchContainer.tsx
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { SearchForm, List } from './index';
 import Styles from './styles';
 import Config from '../../config';
-import { TUserData } from '../../_types';
+import { TUserData, TRepos } from '../../_types';
 
 type State = {
   user: TUserData | null;
   isLoading: boolean;
+  repos: TRepos;
 };
 
 class Searchcontainer extends Component<State> {
   state: State = {
     user: null,
     isLoading: false,
+    repos: null,
   };
 
   // FUNCTION FOR GETTING SEARCHED USERS
@@ -21,7 +23,19 @@ class Searchcontainer extends Component<State> {
     this.setState({ isLoading: true });
     fetch(url)
       .then(response => response.json())
-      .then(data => this.setState({ user: data, isLoading: false }));
+      .then(data =>
+        this.setState({ user: data, isLoading: false }, () => {
+          this.getRepos(search); // CALLING REPOS API
+        }),
+      );
+  };
+
+  // FUNCTION FOR GETTING USERS REPO
+  getRepos = (search: string) => {
+    const url = `${Config.baseURL}users/${search}/repos`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ repos: data }));
   };
 
   // ONCHANGE FUNCTION FOR INPUT
